7c3eb8e5f71002c977d41f76dd61706a4e2de259
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-05-07 17:02:28 +0530

    earth-quake api added

--- src/components/earthquake/Earthmore.jsx
+++ src/components/earthquake/Earthmore.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const Earthmore = () => {
-  return (
-    <div>earthmore</div>
-  )
+function Earthmore() {
+  useEffect(() => {
+    fetch("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2022-05-01&endtime=2023-05-01&minmagnitude=4&latitude=8.4&longitude=77.5&maxradiuskm=2000")
+    .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []);
+
+  return <div>Fetching earthquake data...</div>;
 }
 
-export default Earthmore
+export default Earthmore;
--- src/components/earthquake/Earthquake.jsx
+++ src/components/earthquake/Earthquake.jsx
@@ -1,16 +1,68 @@
-import React from 'react'
-import Footer from '../Footer'
-import Header from '../Header'
-import Navbar from '../Navbar'
+import React, { useState, useEffect } from 'react';
+import Footer from '../Footer';
+import Header from '../Header';
+import Navbar from '../Navbar';
+import './Earthquake.css'
+import { Card, Container, Row, Col } from "react-bootstrap";
 
-const Earthquake = () => {
-  return (
-    <div>
-        <Header/>
-        <Navbar/>
-        <Footer/>
-    </div>
-  )
+
+function Earthquake() {
+    const [data, setData] = useState([]);
+    const [currentPosition, setCurrentPosition] = useState(null);
+    useEffect(() => {
+
+        fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2020-07-01&endtime=2023-05-01&minmagnitude=4&latitude=28.6&longitude=77.2&maxradiuskm=300`)
+        .then(response => response.json())
+        .then(data => setData(data.features))
+        .then((data) => console.log(data))
+        .catch(error => console.log(error));
+        // navigator.geolocation.getCurrentPosition(
+        //     (position) => {
+        //         setCurrentPosition(position);
+        //         console.log(position)
+
+        //         fetch(`https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2023-05-01&minmagnitude=4&latitude=${currentPosition.coords.latitude}&longitude=${currentPosition.coords.longitude}&maxradiuskm=300`)
+        //             .then(response => response.json())
+        //             .then(data => setData(data.features))
+        //             .then((data) => console.log(data))
+        //             .catch(error => console.log(error));
+        //     },
+        //     (error) => {
+              
+        //         console.log(error);
+        //     }
+        // );
+
+
+    }, []);
+
+    return (
+        <div className='earth-quake-box'>
+            <Header/>
+            <Navbar/>
+            <Container className="eq">
+      <h1 className="text-center my-5"> Recent Earthquake Analytics of India</h1>
+      <Row>
+        {data.map((quake) => (
+          <Col sm={12} md={6} lg={4} key={quake.id}>
+            <Card className="my-3">
+              <Card.Body>
+                <Card.Title>{quake.properties.place}</Card.Title>
+                <Card.Text>Magnitude: {quake.properties.mag}</Card.Text>
+                <Card.Text>
+                  Time: {new Date(quake.properties.time).toLocaleString()}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+     
+    </Container>
+    <h6>Wants to know more?</h6>
+            <Footer/>
+        </div>
+    );
 }
 
-export default Earthquake
+export default Earthquake;
